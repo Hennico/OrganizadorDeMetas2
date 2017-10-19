fe464e4861ddbc223529c700fbd9d27d21feff15
FontiLucas <fontilucas@hotmail.com>
2017-10-19 01:28:46 -0300

    comitado la maldita funcion

--- grails-app/domain/organizadordemetas/Meta.groovy
+++ grails-app/domain/organizadordemetas/Meta.groovy
@@ -7,9 +7,9 @@ class Meta {
 		objetivo = new Objetivo()
   }
 
-	public void inicializar(String nombre, String descripcion){
+	/*public void inicializar(String nombre, String descripcion){
 		objetivo.inicializar(nombre, descripcion, Obligatoriedad.NECESARIO)
-  }
+  }*/
 
 
   static constraints = {
